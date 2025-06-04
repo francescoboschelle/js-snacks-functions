d64b43a55e364055d189536641edec1c8f434e3f
francescoboschelle <francibossch@gmail.com>
2025-06-04 13:20:37 +0200

    Added snack 04 with number of vocals function

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,23 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
 
 // Dichiara la funzione qui.
 
+function numOfVocals(word) {
+  const vocals = ["a", "e", "i", "o", "u"];
+  const newArr = [];
+  for (let i = 0; i < word.length; i++) {
+    if (vocals.includes(word.charAt(i))) {
+      newArr.push(word.charAt(i));
+    }
+  }
+
+  return newArr;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(numOfVocals(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
